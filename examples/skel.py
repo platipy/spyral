a3a3b623d583e89317c2ac78c509078d4936a9e7
Robert Deaton <rdeaton@udel.edu>
2011-09-19 19:16:25 -0400

    Add documentation to the skeleton
    Signed-off-by: Robert Deaton <rdeaton@udel.edu>

--- examples/skel.py
+++ examples/skel.py
@@ -3,9 +3,20 @@ import spyral
 
 SIZE = (640, 480)
 BG_COLOR = (0, 0, 0)
-        
+
 class Game(spyral.scene.Scene):
+    """
+    A Scene represents a distinct state of your game. They could be menus,
+    different subgames, or any other things which are mostly distinct.
+    
+    A Scene should define two methods, update and render.
+    """
     def __init__(self):
+        """
+        The __init__ message for a scene should set up the camera(s) for the
+        scene, and setup groups and other structures which are needed for the
+        scene.
+        """
         spyral.scene.Scene.__init__(self)
         self.camera = spyral.director.camera()
         self.group = spyral.sprite.Group(self.camera)
@@ -15,15 +26,25 @@ class Game(spyral.scene.Scene):
         # More setup here
                 
     def render(self):
+        """
+        The render function should call .draw() on the scene's group(s) and
+        camera(s). Unless your game logic should be bound by framerate,
+        logic should go elsewhere.
+        """
         self.group.draw()
         self.camera.draw()
         
     def update(self, tick):
-        # Possibly some event handling in here
+        """
+        The update function should contain or call out to all the logic.
+        Here is where group.update() should be called for the groups, where
+        event handling should be taken care of, etc.
+        """
         self.group.update()
 
 if __name__ == "__main__":
-    spyral.init()
-    spyral.director.init(SIZE)
-    spyral.director.push(Game())
-    spyral.director.run()
+    spyral.init() # Always call spyral.init() first
+    spyral.director.init(SIZE) # the director is the manager for your scenes
+    spyral.director.push(Game()) # push means that this Game() instance is
+                                 # on the stack to run
+    spyral.director.run() # This will run your game. It will not return.
