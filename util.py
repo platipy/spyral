05a219f8a504a02ce4deb4acb52e4604ac808463
Robert Deaton <rdeaton@udel.edu>
2011-11-29 22:22:41 -0500

    Tentatively add the spritesheet code. May need to have the license changed/be rewritten.
    Signed-off-by: Robert Deaton <rdeaton@udel.edu>

--- util.py
+++ util.py
@@ -21,7 +21,42 @@ def load_image(path, colorkey=None):
         print "Cannot load image:", path
         raise SystemExit, message
     if colorkey is not None:
-        if colorkey is -1:
+        if colorkey == -1:
             colorkey = image.get_at((0,0))
-        image.set_colorkey(colorkey, pygame.RLEACCEL)
+        image.set_colorkey(colorkey)
     return image.convert_alpha()
+    
+class Spritesheet(object):
+    """
+    http://pygame.org/wiki/Spritesheet
+    """
+    def __init__(self, filename, colorkey=None):
+        try:
+            self.sheet = pygame.image.load(filename)
+        except pygame.error, message:
+            print 'Unable to load spritesheet image:', filename
+            raise SystemExit, message
+    def image_at(self, rectangle, colorkey = None):
+        "Loads image from x,y,x+offset,y+offset"
+        rect = pygame.Rect(rectangle)
+        image = pygame.Surface(rect.size)
+        image.blit(self.sheet, (0, 0), rect)
+        if colorkey is not None:
+            if colorkey is -1:
+                colorkey = image.get_at((0,0))
+            image.set_colorkey(colorkey, pygame.RLEACCEL)
+        return image.convert_alpha()
+    def images_at(self, rects, colorkey = None):
+        """
+        Loads multiple images from a list of coordinates, returns them as a list
+        """
+        return [self.image_at(rect, colorkey) for rect in rects]
+    def load_strip(self, rect, image_count, colorkey = None):
+        """
+        Loads a strip of images and returns them as a list
+        """
+        tups = [(rect[0]+rect[2]*x, rect[1], rect[2], rect[3])
+                for x in range(image_count)]
+        return self.images_at(tups, colorkey)
+    def unload(self):
+        self.sheet= None;    
