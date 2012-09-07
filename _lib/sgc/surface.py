b9c481e31f03e12e53fb58cd7a7f4ddfa039400d
acbart <acbart@vt.edu>
2012-09-07 15:00:15 -0400

    Initial SGC addition
    Lot's of 2.5 inconsistencies, and of course it's not actually compatible
    with Spyral yet. No examples of it in use either.

---
+++ _lib/sgc/surface.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+#Copyright (C) 2010-2012  Sam Bull
+
+"""
+Screen class to store rect information with the screen and setup the toolkit.
+
+"""
+
+import pygame.display
+from pygame.locals import *
+
+import widgets._locals
+
+class Screen(object):
+    """
+    Class for the screen.
+
+    This must be used instead of ``pygame.display.set_mode()``.
+
+    Attributes:
+      image: The pygame.display screen.
+      rect: ``pygame.Rect`` containing screen size.
+
+    """
+
+    __slots__ = ("rect", "image", "_opengl")
+
+    def __init__(self, size, flags=0, depth=0):
+        """
+        Args:
+          size, flags, depth: Arguments for pygame.display.set_mode()
+
+        """
+        self.rect = Rect((0,0), size)
+        self.image = pygame.display.set_mode(size, flags, depth)
+        self._opengl = flags & OPENGL
+        widgets._locals.SCREEN = self
+        widgets._locals.Font.set_fonts()
+
+    def __getattr__(self, attr):
+        """Redirect attribute access to self.image"""
+        if attr != "image":
+            return getattr(self.image, attr)
+        raise AttributeError("image")
