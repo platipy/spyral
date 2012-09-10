6dca9dfca07248ab0ace4d7936c86070bf94b35d
Robert Deaton <rdeaton@udel.edu>
2012-09-10 15:44:37 -0400

    Add two new animators.

--- animations.py
+++ animations.py
@@ -1,3 +1,4 @@
+import math
 """
 All animators should be functions which take the input time as a number
 from 0 to 1 and return a new value for the property they are animating.
@@ -10,3 +11,16 @@ def Linear(start, finish):
     def linear_animator(dt):
         return (finish-start)*(dt)+start
     return linear_animator
+    
+def Iterate(items, times = 1):
+    def iterate_animator(dt):
+        # We preturb the result slightly negative so that it ends on
+        # the last frame instead of looping back to the first
+        i = math.floor(dt*len(items)*times-0.00000001) 
+        return items[int(i % len(items))]
+    return iterate_animator
+    
+def Sin(amplitude = 1.0, phase = 0, end_phase = 2.0*math.pi):
+    def sin_animator(dt):
+        return amplitude*math.sin(phase + dt*(2.0*math.pi-phase))
+    return sin_animator
