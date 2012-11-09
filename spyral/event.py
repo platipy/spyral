f7490c891ad743a8de7ec65a5d4f6bfef1017ec3
Robert Deaton <rdeaton@udel.edu>
2012-11-08 22:00:36 -0500

    First pass at a new event handling system.
    Signed-off-by: Robert Deaton <rdeaton@udel.edu>

--- spyral/event.py
+++ spyral/event.py
@@ -1,4 +1,5 @@
 import pygame
+import operator
 try:
     import json
 except ImportError:
@@ -7,6 +8,7 @@ import spyral
 import os
 import random
 import base64
+from collections import defaultdict
 
 class EventDict(dict):
     def __getattr__(self, attr):
@@ -117,7 +119,90 @@ class LiveEventHandler(EventHandler):
         if self._save:
             self._file.close()
 
-
+class EventManager(object):
+    def __init__(self):
+        """
+        The event manager's task is to take events and send them to
+        listeners which have registered to receive events.
+        
+        Events are any object which have a type attribute, which is a
+        string. There are a number of built-in events.
+        
+        A listener is an object which has a handle_event method which
+        takes one event as an argument.
+        
+        If handle_event returns True, the manager does not pass the
+        event to any other listeners. To ensure the right listeners
+        handle events, ensure that their priority is set accordingly.
+        
+        
+        """
+        self._listeners = defaultdict(lambda : [])
+        self._events = []
+        self._busy = False
+        
+    def register_listener(self, listener, event_types, priority = 5):
+        # We may switch to bisect here at some point, but for now, we'll just resort
+        for event_type in event_types:
+            self._listeners[event_type].append((listener, priority))
+            self._listeners[event_type] = sorted(self._listeners[event_type], key=operator.itemgetter(1), reverse = True)
+            
+    def unregister_listener(self, listener, event_types = None, priority = None):
+        """
+        Remove the listener for the given event types and priority. If
+        event_types is None, remove it for all event types, and if 
+        priority is None, remove it for any priority.
+        """
+        if event_types is None:
+            event_types = list(self._listeners.iterkeys())
+        for e in event_types:
+            self._listeners[e] = [l for l in self._listeners[e] if not (l[0] is listener and l[1] == priority)]
+        
+    def send_events(self, events):
+        """
+        Sends an event to the relevant listeners.
+        
+        If events are currently being processed by the manager, the
+        events are added to the list of events to be sent out.
+        """
+        if not self._busy:
+            self._busy = True
+            self._events.extend(events)
+            while len(self._events) > 0:
+                events = self._events
+                self._events = []
+                for event in events:
+                    # Make sure we avoid futzing with things while iterating
+                    listeners = self._listeners[event.type]
+                    for listener in listeners:
+                        r = listener[0].handle_event(event)
+                        if r is True:
+                            break
+            self._busy = False
+        else:
+           self._events.extend(events) 
+        
+        
+    def send_event(self, event, immediate = False):
+        """
+        Sends an event to the relevant listeners.
+        
+        If events are currently being processed by the manager, the
+        event is added to the list of events to be sent out, unless
+        immediate is set to True, in which case it is sent immediately.
+        Use immediate sparingly, as it may lead to a large stack if
+        events trigger more immediate calls.
+        """
+        if not self._busy or immediate is True:
+            # Make sure we avoid futzing with things while iterating
+            listeners = self._listeners[event.type]
+            for listener in listeners:
+                r = listener[0].handle_event(event)
+                if r is True:
+                    break
+        else:
+            self._events.append(event)
+        
 class ReplayEventHandler(EventHandler):
     def __init__(self, input_file):
         EventHandler.__init__(self)
