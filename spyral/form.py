ea4974b667386fce8f5181c66a4a5453fd080db6
Robert Deaton <rdeaton@udel.edu>
2012-11-17 16:01:31 -0500

    Get rid of stupid inheritance crap
    Signed-off-by: Robert Deaton <rdeaton@udel.edu>

--- spyral/form.py
+++ spyral/form.py
@@ -112,15 +112,15 @@ class TextInputWidget(spyral.AggregateSprite, spyral.FormWidget):
                 # set selected_pos to mouse_position
                 pass
 
-class ButtonWidget(spyral.Sprite, FormWidget):
+class ButtonWidget(spyral.Sprite):
     def __init__(self, text, style = None):
         pass
         
-class ToggleButtonWidget(spyral.Sprite, FormWidget):
+class ToggleButtonWidget(spyral.Sprite):
     def __init__(self, text, style = None):
         pass
 
-class CheckboxWidget(spyral.Sprite, FormWidget):
+class CheckboxWidget(spyral.Sprite):
     def __init__(self, text, style = None):
             pass
 
@@ -128,7 +128,7 @@ class RadioButton(spyral.Sprite):
     def __init__(self, value, style = None):
         pass
         
-class RadioGroup(FormWidget):
+class RadioGroup(object):
     def __init__(self, *buttons):
         pass
 
