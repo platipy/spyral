c830d67e6b640791e1a8d9117c75ca146ea5d6c8
Robert Deaton <rdeaton@udel.edu>
2012-11-24 20:54:08 -0500

    Remove poor way of doing default styles
    Signed-off-by: Robert Deaton <rdeaton@udel.edu>

--- spyral/core.py
+++ spyral/core.py
@@ -1,11 +1,16 @@
 import spyral
 import pygame
 
+_inited = False
+
 def init():
+    global _inited
+    if _inited:
+        return
+    _inited = True
     spyral.event.init()
     pygame.init()
     pygame.font.init()
-
 
 def quit():
     pygame.quit()
