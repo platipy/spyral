08f8d3918eb6da69feecea49e079c679f0917068
Robert Deaton <rdeaton@udel.edu>
2013-03-12 13:39:00 -0400

    Fix up initialization to set up the parser early, only create one instance. Also don't bother initializing pygame.mixer

--- spyral/_style.py
+++ spyral/_style.py
@@ -4,25 +4,32 @@ import string
 from collections import defaultdict
 from ast import literal_eval
 
+parser = None
+
+def init():
+    global parser
+    parser = StyleParser()
+    parser.parser = parsley.makeGrammar(open(spyral._get_spyral_path() + 'resources/style.parsley', "r").read(),
+                                          {"string": string,
+                                           "parser": parser,
+                                           "leval": literal_eval})
+
+def parse(style, scene):
+    parser.scene = scene
+    parser.parse(style)
+
 class StyleParser(object):
     def __init__(self):
-        self.symbols = {}
-        # Todo: Maybe cache the parser generation, if possible?
-        self.parser = parsley.makeGrammar(open(spyral._get_spyral_path() + 'resources/style.parsley', "r").read(),
-                                          {"string": string,
-                                           "parser": self,
-                                           "leval": literal_eval})
+        self.scene = None
         self.classes = []
-        self.properties = defaultdict(lambda: {})
 
     def assign(self, identifier, value):
-        print 'Assign %s = %s' % (identifier, value)
-        self.symbols[identifier] = value
+        self.scene._style_symbols[identifier] = value
 
     def lookup(self, identifier):
-        if identifier not in self.symbols:
+        if identifier not in self.scene._style_.symbols:
             raise NameError("%s is not a previously defined name in the styles" % identifier)
-        return self.symbols[identifier]
+        return self.scene._style_symbols[identifier]
 
     def calculate(self, ret, ops):
         for op in ops:
@@ -45,11 +52,11 @@ class StyleParser(object):
     def set_property(self, property, value):
         for cls in self.classes:
             if property == 'inherit':
-                if value not in self.properties:
+                if value not in self.scene._style_properties:
                     raise ValueError("Requested to inherit from '%s' which has no style" % value)
-                self.properties[cls].update(self.properties[value])
+                self.scene._style_properties[cls].update(self.properties[value])
             else:
-                self.properties[cls][property] = value
+                self.scene._style_properties[cls][property] = value
 
     def parse(self, style):
         parse = self.parser(style).all()
