da335bf0d394be6dcfa36b12d8d4fff05bf64c5d
Austin Cory Bart <acbart@udel.edu>
2013-03-23 19:47:09 -0400

    Removed random prin

--- spyral/event.py
+++ spyral/event.py
@@ -67,7 +67,6 @@ def _pygame_to_spyral(event):
         setattr(e, "type", type.split(".")[-1])
     if type.startswith('input.keyboard'):
         k = pygame.key.name(event.key).replace(' ', '_').replace('.', 'dot')
-        print k
         type += '.' + k
         
     return (type, e)
