e9c917d904f8a99bd2101e9a55f67f873977e848
Robert Deaton <rdeaton@udel.edu>
2013-03-26 15:41:22 -0400

    Move offset calculation code into a utility function

--- spyral/sprite.py
+++ spyral/sprite.py
@@ -105,30 +105,9 @@ class Sprite(object):
         if self.image is None:
             return
         size = self._scale * self._image.get_size()
-        w = size[0]
-        h = size[1]
-        a = self._anchor
 
-        if a == 'topleft':
-            offset = (0, 0)
-        elif a == 'topright':
-            offset = (w, 0)
-        elif a == 'midtop':
-            offset = (w / 2., 0)
-        elif a == 'bottomleft':
-            offset = (0, h)
-        elif a == 'bottomright':
-            offset = (w, h)
-        elif a == 'midbottom':
-            offset = (w / 2., h)
-        elif a == 'midleft':
-            offset = (0, h / 2.)
-        elif a == 'midright':
-            offset = (w, h / 2.)
-        elif a == 'center':
-            offset = (w / 2., h / 2.)
-        else:
-            offset = a * spyral.Vec2D(-1, -1)
+        offset = spyral.util.anchor_offset(self._anchor, size[0], size[1])
+
         self._offset = spyral.Vec2D(offset) - self._transform_offset
             
     def _recalculate_transforms(self):
---
+++ spyral/util.py
@@ -0,0 +1,27 @@
+import spyral
+
+def anchor_offset(anchor, width, height)
+    w = width
+    h = height
+    a = anchor
+    if a == 'topleft':
+        offset = (0, 0)
+    elif a == 'topright':
+        offset = (w, 0)
+    elif a == 'midtop':
+        offset = (w / 2., 0)
+    elif a == 'bottomleft':
+        offset = (0, h)
+    elif a == 'bottomright':
+        offset = (w, h)
+    elif a == 'midbottom':
+        offset = (w / 2., h)
+    elif a == 'midleft':
+        offset = (0, h / 2.)
+    elif a == 'midright':
+        offset = (w, h / 2.)
+    elif a == 'center':
+        offset = (w / 2., h / 2.)
+    else:
+        offset = a * spyral.Vec2D(-1, -1)
+    return spyral.Vec2D(a)
