312ba7eabce3d3d7807abefaf61a8a4e93658d37
acbart <acbart@vt.edu>
2014-02-03 09:33:51 -0500

    spyral.__init__ passes pylint.

--- spyral/__init__.py
+++ spyral/__init__.py
@@ -9,11 +9,11 @@ __author__ = 'Robert Deaton'
 from types import ModuleType
 import sys
 
-import compat
+import spyral.compat
 import pygame
 
 # import mapping to objects in other modules
-all_by_module = {
+ALL_BY_MODULE = {
     'spyral.debug' : ['DebugText'],
     'spyral.sprite' : ['Sprite'],
     'spyral.scene' : ['Scene'],
@@ -22,57 +22,63 @@ all_by_module = {
     'spyral.rect' : ['Rect'],
     'spyral.animation' : ['Animation'],
     'spyral.core' : ['init', 'quit', '_get_executing_scene'],
-    'spyral.cursor' : ['set_cursor', 'MonochromeCursor', 'ColorCursor', 'cursors'],
+    'spyral.cursor' : ['set_cursor', 'MonochromeCursor', 'ColorCursor',
+                       'cursors'],
     'spyral.font' : ['Font'],
     'spyral.clock' : ['GameClock'],
-    'spyral.event' : ['keys', 'mods', 'queue', 'Event', 'EventHandler', 'LiveEventHandler'],
+    'spyral.event' : ['keys', 'mods', 'queue', 'Event',
+                      'EventHandler', 'LiveEventHandler'],
     'spyral.form' : ['Form'],
     'spyral.dev' : ['_get_spyral_path'],
     'spyral.actor' : ['Actor'],
     'spyral.util' : ['anchor_offset'],
-    'spyral.exceptions': ['SceneHasNoSizeError', 'NotStylableError', 'NoImageError', 'BackgroundSizeError', 'LayersAlreadySetError', 'UnusedStyleWarning'],
+    'spyral.exceptions': ['SceneHasNoSizeError', 'NotStylableError',
+                          'NoImageError', 'BackgroundSizeError',
+                          'LayersAlreadySetError', 'UnusedStyleWarning'],
     'spyral.view': ['View']
 }
 
-attribute_modules = frozenset(['memoize', 'point', 'exceptions', 'easing', 'event', '_lib', 'font', 'form', 'director', 'sprite', '_style', 'widgets', 'cursor', 'util'])
+ATTRIBUTE_MODULES = frozenset(['memoize', 'point', 'exceptions', 'easing',
+                               'event', '_lib', 'font', 'form', 'director',
+                               'sprite', '_style', 'widgets', 'cursor', 'util'])
 
-object_origins = {}
-for module, items in all_by_module.iteritems():
+OBJECT_ORIGINS = {}
+for module, items in ALL_BY_MODULE.iteritems():
     for item in items:
-        object_origins[item] = module
+        OBJECT_ORIGINS[item] = module
 
-class module(ModuleType):
+class SpyralModule(ModuleType):
     """Automatically import objects from the modules."""
 
     def __getattr__(self, name):
-        if name in object_origins:
-            module = __import__(object_origins[name], None, None, [name])
-            for extra_name in all_by_module[module.__name__]:
-                setattr(self, extra_name, getattr(module, extra_name))
-            return getattr(module, name)
-        elif name in attribute_modules:
+        if name in OBJECT_ORIGINS:
+            sub_module = __import__(OBJECT_ORIGINS[name], None, None, [name])
+            for extra_name in ALL_BY_MODULE[sub_module.__name__]:
+                setattr(self, extra_name, getattr(sub_module, extra_name))
+            return getattr(sub_module, name)
+        elif name in ATTRIBUTE_MODULES:
             __import__('spyral.' + name)
         return ModuleType.__getattribute__(self, name)
 
     def __dir__(self):
         """Just show what we want to show."""
-        result = list(new_module.__all__)
+        result = list(NEW_MODULE.__all__)
         result.extend(('__file__', '__path__', '__doc__', '__all__',
                        '__docformat__', '__name__', '__path__',
                        '__package__', '__version__'))
         return result
 
 # keep a reference to this module so that it's not garbage collected
-old_module = sys.modules['spyral']
+OLD_MODULE = sys.modules['spyral']
 
 # setup the new module and patch it into the dict of loaded modules
-new_module = sys.modules['spyral'] = module('spyral')
-new_module.__dict__.update({
+NEW_MODULE = sys.modules['spyral'] = SpyralModule('spyral')
+NEW_MODULE.__dict__.update({
     '__file__': __file__,
     '__package__': 'spyral',
     '__path__': __path__,
     '__doc__': __doc__,
     '__version__': __version__,
-    '__all__': tuple(object_origins) + tuple(attribute_modules),
+    '__all__': tuple(OBJECT_ORIGINS) + tuple(ATTRIBUTE_MODULES),
     '__docformat__': 'restructuredtext en'
 })
