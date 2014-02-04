034fa73237e4857f9f1c3256b71f8a7addd9eadd
acbart <acbart@vt.edu>
2014-02-04 06:06:48 -0500

    pylint compat.py

--- spyral/compat.py
+++ spyral/compat.py
@@ -4,15 +4,19 @@ if sys.version_info[0] == 2 and sys.version_info[1] == 5:
     _property = property
 
     class property(property):
+        """
+        Custom class meant to implement Python 2.6+ property functionality into
+        Python 2.5.
+        """
         def __init__(self, fget, *args, **kwargs):
             self.__doc__ = fget.__doc__
             super(property, self).__init__(fget, *args, **kwargs)
 
         def setter(self, fset):
             cls_ns = sys._getframe(1).f_locals
-            for k, v in cls_ns.iteritems():
-                if v == self:
-                    propname = k
+            for key, value in cls_ns.iteritems():
+                if value == self:
+                    propname = key
                     break
             cls_ns[propname] = property(self.fget, fset,
                                         self.fdel, self.__doc__)
