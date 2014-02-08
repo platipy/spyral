b7518bca99be29a9050b7c9dbf95c8632168f12b
acbart <acbart@vt.edu>
2014-02-08 16:00:21 -0500

    Partial cleanup and documentation of _style.py

--- spyral/_style.py
+++ spyral/_style.py
@@ -1,3 +1,6 @@
+"""Styling offers a way to offshore static data from your game into a .spys
+file, improving the separation of code and data."""
+
 import spyral
 import parsley
 import string
@@ -6,29 +9,57 @@ from ast import literal_eval
 parser = None
 
 def init():
+    """
+    Initializes the Styler.
+    """
     global parser
     parser = StyleParser()
-    parser.parser = parsley.makeGrammar(open(spyral._get_spyral_path() + 'resources/style.parsley', "r").read(),
-                                          {"string": string,
-                                           "parser": parser,
-                                           "leval": literal_eval,
-                                           "Vec2D": spyral.Vec2D})
+    style_file = open(spyral._get_spyral_path() +
+                      'resources/style.parsley').read()
+    parser.parser = parsley.makeGrammar(style_file,
+                                        {"string": string,
+                                         "parser": parser,
+                                         "leval": literal_eval,
+                                         "Vec2D": spyral.Vec2D})
 
 def parse(style, scene):
+    """
+    Parses a style and applies it to the scene.
+
+    :param str style: The style definition
+    :param scene: The scene to apply this definition to.
+    :type scene: :class:`Scene <spyral.Scene>`
+    """
     parser.scene = scene
     parser.parse(style)
 
 class StyleParser(object):
+    """
+    The style parser is a single instance class that converts a style file into
+    attributes to be applied to an object (e.g., a Scene, View, or Sprite).
+    """
     def __init__(self):
         self.scene = None
         self.classes = []
 
     def assign(self, identifier, value):
+        """
+        Assigns the identifier to the particular style symbol.
+
+        :param str identifier: The identifier (?)
+        :param ? value: ?
+        """
         self.scene._style_symbols[identifier] = value
 
     def lookup(self, identifier):
+        """
+        Return the style symbols associated with this identifier in the scene.
+
+        :param str identifier: The identifier (e.g., ?)
+        """
         if identifier not in self.scene._style_symbols:
-            raise NameError("%s is not a previously defined name in the styles" % identifier)
+            raise NameError("%s is not a previously defined name in the styles"
+                            % identifier)
         return self.scene._style_symbols[identifier]
 
     def calculate(self, ret, ops):
@@ -53,7 +84,8 @@ class StyleParser(object):
         for cls in self.classes:
             if property == 'inherit':
                 if value not in self.scene._style_properties:
-                    raise ValueError("Requested to inherit from '%s' which has no style" % value)
+                    raise ValueError(("Requested to inherit from "
+                                     "'%s' which has no style") % value)
                 self.scene._style_properties[cls].update(self.properties[value])
             else:
                 self.scene._style_properties[cls][property] = value
