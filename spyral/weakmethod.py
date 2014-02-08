2cc2f3456e6eda808f8839b6f5ab8690b3d04916
acbart <acbart@vt.edu>
2014-02-08 15:05:15 -0500

    Doc'd and pylinted weakmethod.py

--- spyral/weakmethod.py
+++ spyral/weakmethod.py
@@ -1,31 +1,57 @@
+"""
+This magic was taken from
+http://code.activestate.com/recipes/81253-weakmethod/#c4
+
+This module provides classes and methods for weakly referencing functions and
+bound methods; it turns out this is a non-trivial problem.
+"""
+
 import weakref
 
-class WeakMethodBound :
-    def __init__( self , f ) :
-        self.f = f.im_func
-        self.c = weakref.ref( f.im_self )
-    def _f(self):
-        return self.f
-    method = property(_f)
-    def __call__( self , *arg ) :
-        if self.c() == None :
-            raise TypeError , 'Method called on dead object'
-        return apply( self.f , ( self.c() , ) + arg )
+class WeakMethodBound(object):
+    """
+    Holds a weak reference to a bound method for an object.
 
-class WeakMethodFree :
-    def __init__( self , f ) :
-        self.f = weakref.ref( f )
-    def _f(self):
-        return self.f()
-    method = property(_f)
-    def __call__( self , *arg ) :
-        if self.f() == None :
-            raise TypeError , 'Function no longer exist'
-        return apply( self.f() , arg )
+    .. attribute::method
 
-def WeakMethod( f ) :
+        The function being called.
+    """
+    def __init__(self, func):
+        self.func = func.im_func
+        self.weak_object_ref = weakref.ref(func.im_self)
+    def _func(self):
+        return self.func
+    method = property(_func)
+    def __call__(self, *arg):
+        if self.weak_object_ref() == None:
+            raise TypeError('Method called on dead object')
+        return apply(self.func, (self.weak_object_ref(), ) + arg)
+
+class WeakMethodFree(object):
+    """
+    Holds a weak reference to an unbound function.
+
+    .. attribute::method
+
+        The function being called.
+    """
+    def __init__(self, func):
+        self.func = weakref.ref(func)
+    def _func(self):
+        return self.func()
+    method = property(_func)
+    def __call__(self, *arg):
+        if self.func() == None:
+            raise TypeError('Function no longer exist')
+        return apply(self.func(), arg)
+
+def WeakMethod(func):
+    """
+    Attempts to create a weak reference to this function; only bound methods
+    require a weakreference.
+    """
     try:
-        f.im_func
-    except AttributeError :
-        return f
-    return WeakMethodBound( f )
+        func.im_func
+    except AttributeError:
+        return func
+    return WeakMethodBound(func)
